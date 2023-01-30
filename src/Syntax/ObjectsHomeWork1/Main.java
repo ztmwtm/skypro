72db9bd0f69d79ed06151797b1dcf6af8e98ad9a
Vladimir Kudriavtsev <ztmwtm@gmail.com>
2023-01-31 00:29:48 +0300

    ObjectsHomeWork1 complete

---
+++ src/Syntax/ObjectsHomeWork1/Main.java
@@ -0,0 +1,18 @@
+package Syntax.ObjectsHomeWork1;
+
+public class Main {
+    public static void main(String[] args) {
+        Author edgarPo = new Author("Edgar", "Poe");
+        Book theBlackCat = new Book("The Black Cat", edgarPo, 1843);
+
+        Author lionFat = new Author("Лев", "Толстой");
+        Book theWarAndPiece = new Book("Война и мир", lionFat, 1868);
+
+        System.out.println(theWarAndPiece);
+
+        System.out.println(theBlackCat);
+        theBlackCat.setPublicationYear(2003);
+        System.out.println(theBlackCat);
+
+    }
+}
